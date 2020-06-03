f040c51e57c664d0e5df1ece9d7dafc3417ff8b3
Ricardo Quijas <ricardo.quijass@gmail.com>
2020-06-02 20:31:52 -0500

    Agregando los archivos recientes

--- schoolar_age.py
+++ schoolar_age.py
@@ -1,16 +1,47 @@
 
 edad = int(input("Ingresa por favor tu edad actual: "))
+promedio = float(input("Teclea tu promedio: "))
+
+
+
 
 if edad < 6:
     print('Kinder')
+    print('Tú no aplicas')
 elif edad >= 6 and edad < 12:
     print('Primaria')
+    if promedio >= 9.5:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
 elif edad >= 12 and edad < 15:
     print('Secundaria')
+    if promedio > 9.5:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
 elif edad >= 15 and edad < 18:
     print('Bachillerato')
+    if promedio >= 8.5:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
+
 elif edad >= 18 and edad < 23:
     print('Universidad')
+    if promedio >= 8:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
+
 else:
     print('Profesionista')
 
