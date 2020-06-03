fb5a411414bc672dfd966773b241cb62ccb4ea31
Ricardo Quijas <ricardo.quijass@gmail.com>
2020-06-02 20:33:23 -0500

    Archivos actualizados de retos

---
+++ age_and_promedio.py
@@ -0,0 +1,39 @@
+if edad < 6:
+    print('Kinder')
+    print('Tú no aplicas')
+elif edad >= 6 and edad < 12:
+    print('Primaria')
+    if promedio >= 9.5:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
+elif edad >= 12 and edad < 15:
+    print('Secundaria')
+    if promedio > 9.5:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
+elif edad >= 15 and edad < 18:
+    print('Bachillerato')
+    if promedio >= 8.5:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
+
+elif edad >= 18 and edad < 23:
+    print('Universidad')
+    if promedio >= 8:
+       print('Promedio aceptable')
+    else:
+       print('Promedio no aceptable')
+
+
+
+else:
+    print('Profesionista')
