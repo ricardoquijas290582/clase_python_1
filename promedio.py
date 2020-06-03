afd937d893eaa22f73f33a875e97a9d87286f0ab
Ricardo Quijas <ricardo.quijass@gmail.com>
2020-06-02 19:43:43 -0500

    Agregando el archivo de promedio

---
+++ promedio.py
@@ -0,0 +1,13 @@
+nombre = input('Nombre? ')
+print("Me alegro de conocerle " + nombre)
+
+cal_1 = int(input("Ingresa calif 1: "))
+cal_2 = int(input("Ingresa calif 2: "))
+cal_3 = int(input("Ingresa calif 3: "))
+
+promedio = (cal_1 + cal_2 + cal_3) / 3
+
+if promedio >= 7:
+    print('Aprobado!')
+else:
+    print('Reprobado!, suerte para la próxima...')
